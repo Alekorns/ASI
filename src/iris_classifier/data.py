b5a92a0202cb00baf7d21665e9e17b74b9b51066
s28587 <s28587@pjwstk.edu.pl>
2025-11-27 21:25:28 +0100

    Adapt the code to local iris file and initialize DSV

--- src/iris_classifier/data.py
+++ src/iris_classifier/data.py
@@ -1,8 +1,13 @@
-from sklearn.datasets import load_iris
 import pandas as pd
+from pathlib import Path
 
 
-def load_iris_df() -> pd.DataFrame:
+def load_iris_df(path: str = "data/IRIS.csv") -> pd.DataFrame:
 
-    iris = load_iris(as_frame=True)
-    return iris.frame.copy()
+    path = Path(path)
+    df = pd.read_csv(path)
+
+    species_to_id = {name: i for i, name in enumerate(sorted(df["species"].unique()))}
+    df["target"] = df["species"].map(species_to_id)
+
+    return df
